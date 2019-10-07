15c31fea9fc1f0cdb09e99aba0740c77fa4809cc
Evan Brass <evan-brass@protonmail.com>
2019-10-06 21:15:59 -0700

    Finished recreating the template

--- build.rs
+++ build.rs
@@ -1,15 +1,29 @@
-use gl_generator::{Api, Fallbacks, Profile, Registry};
+// Copyright 2015 Brendan Zabarauskas and the gl-rs developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate gl_generator;
+
+use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 use std::env;
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::Path;
 
 fn main() {
-    let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut file = File::create(&Path::new(&out_dir).join("bindings.rs")).unwrap();
 
-    println!("cargo:rerun-if-changed=build.rs");
-
-    let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
-    Registry::new(Api::Gles2, (3, 3), Profile::Core, Fallbacks::All, [])
-        .write_bindings(gl_generator::StructGenerator, &mut file)
+    Registry::new(Api::Gl, (2, 1), Profile::Core, Fallbacks::All, [])
+        .write_bindings(GlobalGenerator, &mut file)
         .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,327 @@
-use glutin::event::{Event, WindowEvent};
+#![allow(dead_code)]
+#![allow(unused_imports)]
+/**
+ * This code is based largely off of two examples / tutorials:
+ * https://github.com/rust-windowing/glutin/blob/master/glutin_examples/examples/window.rs
+ * https://wiki.alopex.li/LearningGfx
+ * 
+ * I've just been trimming it down to what I actually need to then recreate the sample 
+ * project that Dr. Bailey has given us.
+ */
+
+use glutin::event::{Event, WindowEvent, StartCause, ElementState};
 use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
-use glutin::{ContextBuilder, ContextWrapper, PossiblyCurrent};
-use glutin::dpi::LogicalSize;
+use glutin::{GlRequest, ContextBuilder, WindowedContext, PossiblyCurrent};
+use glutin::dpi::{LogicalSize, LogicalPosition};
+use glutin::Api::OpenGl;
+use glutin::event::*;
 
+use cgmath::{Matrix4, Deg, PerspectiveFov, Point3, Vector3};
+
+use std::time::Instant;
 use std::ffi::CStr;
 
-pub mod bindings {
-    pub use self::Gles2 as Gl;
-    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+mod gl {
+	include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
+use gl::types::*;
 
+struct DisplayLists {
+	cube: GLuint,
+	axis: GLuint
+}
+struct ButtonStates {
+	left: ElementState,
+	middle: ElementState,
+	right: ElementState
+}
 struct Demo {
-	wrapped_context: ContextWrapper<PossiblyCurrent, winit::window::Window>,
-	gl: bindings::Gl
+	wrapped_context: WindowedContext<PossiblyCurrent>,
+	last_animate: Option<Instant>,
+	yrot: f32,
+	xrot: f32,
+	scale: f32,
+	display_lists: Option<DisplayLists>,
+	// fog_on: bool
+	button_states: ButtonStates
 }
 
 impl Demo {
-	fn new(wrapped_context: ContextWrapper<PossiblyCurrent, winit::window::Window>) -> Demo {
+	fn new(wrapped_context: WindowedContext<PossiblyCurrent>) -> Demo {
 		println!(
 			"Pixel format of the window's GL context: {:?}",
 			wrapped_context.get_pixel_format()
 		);
 
-		let gl = bindings::Gl::load_with(|ptr| wrapped_context.context().get_proc_address(ptr) as *const _);
+		gl::load_with(|ptr| wrapped_context.context().get_proc_address(ptr) as *const _);
 
-		Demo { wrapped_context, gl }
+		Demo { 
+			wrapped_context,
+			last_animate: None,
+			yrot: 0.0, xrot: 0.0,
+			display_lists: None,
+			scale: 1.0,
+			// fog_on: false
+			button_states: ButtonStates {
+				left: ElementState::Released,
+				middle: ElementState::Released,
+				right: ElementState::Released
+			}
+		}
+	}
+	fn build_display_lists(&mut self) -> DisplayLists {
+		let cube = unsafe {
+			gl::GenLists(1)
+		};
+		unsafe {
+			const DX: f32 = 0.5;
+			const DY: f32 = DX;
+			const DZ: f32 = DY;
+			gl::NewList( cube, gl::COMPILE );
+				gl::Begin( gl::QUADS );
+					gl::Color3f( 0., 0., 1. );
+					gl::Normal3f( 0., 0.,  1. );
+						gl::Vertex3f( -DX, -DY,  DZ );
+						gl::Vertex3f(  DX, -DY,  DZ );
+						gl::Vertex3f(  DX,  DY,  DZ );
+						gl::Vertex3f( -DX,  DY,  DZ );
+					gl::Normal3f( 0., 0., -1. );
+						gl::TexCoord2f( 0., 0. );
+						gl::Vertex3f( -DX, -DY, -DZ );
+						gl::TexCoord2f( 0., 1. );
+						gl::Vertex3f( -DX,  DY, -DZ );
+						gl::TexCoord2f( 1., 1. );
+						gl::Vertex3f(  DX,  DY, -DZ );
+						gl::TexCoord2f( 1., 0. );
+						gl::Vertex3f(  DX, -DY, -DZ );
+					gl::Color3f( 1., 0., 0. );
+					gl::Normal3f(  1., 0., 0. );
+						gl::Vertex3f(  DX, -DY,  DZ );
+						gl::Vertex3f(  DX, -DY, -DZ );
+						gl::Vertex3f(  DX,  DY, -DZ );
+						gl::Vertex3f(  DX,  DY,  DZ );
+					gl::Normal3f( -1., 0., 0. );
+						gl::Vertex3f( -DX, -DY,  DZ );
+						gl::Vertex3f( -DX,  DY,  DZ );
+						gl::Vertex3f( -DX,  DY, -DZ );
+						gl::Vertex3f( -DX, -DY, -DZ );
+					gl::Color3f( 0., 1., 0. );
+					gl::Normal3f( 0.,  1., 0. );
+						gl::Vertex3f( -DX,  DY,  DZ );
+						gl::Vertex3f(  DX,  DY,  DZ );
+						gl::Vertex3f(  DX,  DY, -DZ );
+						gl::Vertex3f( -DX,  DY, -DZ );
+					gl::Normal3f( 0., -1., 0. );
+						gl::Vertex3f( -DX, -DY,  DZ );
+						gl::Vertex3f( -DX, -DY, -DZ );
+						gl::Vertex3f(  DX, -DY, -DZ );
+						gl::Vertex3f(  DX, -DY,  DZ );
+				gl::End( );
+			gl::EndList( );
+		}
+		let axis = unsafe {
+			gl::GenLists( 1 )
+		};
+		unsafe {
+			gl::NewList( axis, gl::COMPILE );
+				gl::LineWidth( 1.0 );
+				gl::Begin(gl::LINE_STRIP);
+				let d = 0.2;
+				let l = 2.0;
+				gl::Vertex3f(0.0, 0.0, 0.0);
+				gl::Vertex3f(0.0, l, 0.0);
+				gl::Vertex3f(-d, l - d, 0.0);
+				gl::Vertex3f(0.0, l, 0.0);
+				gl::Vertex3f(d, l - d, 0.0);
+				gl::End();
+				gl::LineWidth( 1.0 );
+			gl::EndList( );
+		}
+		DisplayLists {
+			cube,
+			axis
+		}
 	}
 	fn init(&mut self) {
 		let version = unsafe {
-			let data = CStr::from_ptr(self.gl.GetString(bindings::VERSION) as *const _)
+			let data = CStr::from_ptr(gl::GetString(gl::VERSION) as *const _)
 				.to_bytes()
 				.to_vec();
 			String::from_utf8(data).unwrap()
 		};
-
 		println!("OpenGL version {}", version);
-
 		unsafe {
-			let vs = self.gl.CreateShader(bindings::VERTEX_SHADER);
-			let vs_src = include_bytes!("shaders/main.glslv");
-			self.gl.ShaderSource(
-				vs,
-				1,
-				// [include_bytes!("shaders/main.glslv").as_ptr() as *const _].as_ptr(),
-				[vs_src.as_ptr() as *const _].as_ptr(),
-				[vs_src.len() as i32].as_ptr(),
-			);
-			self.gl.CompileShader(vs);
+			gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+			gl::Enable(gl::DEPTH_TEST);
+			// since we are using glScalef( ), be sure normals get unitized:
+			gl::Enable( gl::NORMALIZE );
+		}
 
-			let fs = self.gl.CreateShader(bindings::FRAGMENT_SHADER);
-			let fs_src = include_bytes!("shaders/main.glslf");
-			self.gl.ShaderSource(
-				fs,
-				1,
-				// [include_bytes!("shaders/main.glslf").as_ptr() as *const _].as_ptr(),
-				[fs_src.as_ptr() as *const _].as_ptr(),
-				[fs_src.len() as i32].as_ptr(),
-			);
-			self.gl.CompileShader(fs);
+		self.display_lists = Some(self.build_display_lists());
 
-			let program = self.gl.CreateProgram();
-			self.gl.AttachShader(program, vs);
-			self.gl.AttachShader(program, fs);
-			self.gl.LinkProgram(program);
-			self.gl.UseProgram(program);
-			
-			#[rustfmt::skip]
-			static VERTEX_DATA: [f32; 15] = [
-				-0.5, -0.5,  1.0,  0.0,  0.0,
-				0.0,  0.5,  0.0,  1.0,  0.0,
-				0.5, -0.5,  0.0,  0.0,  1.0,
-			];
-
-			let mut vb = std::mem::zeroed();
-			self.gl.GenBuffers(1, &mut vb);
-			self.gl.BindBuffer(bindings::ARRAY_BUFFER, vb);
-			self.gl.BufferData(
-				bindings::ARRAY_BUFFER,
-				(VERTEX_DATA.len() * std::mem::size_of::<f32>())
-					as bindings::types::GLsizeiptr,
-				VERTEX_DATA.as_ptr() as *const _,
-				bindings::STATIC_DRAW,
-			);
-
-			if self.gl.BindVertexArray.is_loaded() {
-				let mut vao = std::mem::zeroed();
-				self.gl.GenVertexArrays(1, &mut vao);
-				self.gl.BindVertexArray(vao);
+	}
+	fn draw_axis(&mut self) {
+		if let Some(ref lists) = self.display_lists {
+			unsafe {
+				// Y-axis label
+				gl::Color3f(0.0, 1.0, 0.0);
+				gl::CallList(lists.axis);
+				gl::Color3f(1.0, 0.0, 0.0);
+				// X-axis label
+				gl::Color3f(1.0, 0.0, 0.0);
+				gl::PushMatrix();
+				gl::Rotatef(90.0, 0.0, 0.0, 1.0);
+				gl::CallList(lists.axis);
+				gl::PopMatrix();
+				// Z-axis label
+				gl::Color3f(0.0, 0.0, 1.0);
+				gl::PushMatrix();
+				gl::Rotatef(90.0, 1.0, 0.0, 0.0);
+				gl::CallList(lists.axis);
+				gl::PopMatrix();
 			}
-
-			let pos_attrib =
-				self.gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
-			let color_attrib =
-				self.gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
-			self.gl.VertexAttribPointer(
-				pos_attrib as bindings::types::GLuint,
-				2,
-				bindings::FLOAT,
-				0,
-				5 * std::mem::size_of::<f32>() as bindings::types::GLsizei,
-				std::ptr::null(),
-			);
-			self.gl.VertexAttribPointer(
-				color_attrib as bindings::types::GLuint,
-				3,
-				bindings::FLOAT,
-				0,
-				5 * std::mem::size_of::<f32>() as bindings::types::GLsizei,
-				(2 * std::mem::size_of::<f32>()) as *const () as *const _,
-			);
-			self.gl.EnableVertexAttribArray(pos_attrib as bindings::types::GLuint);
-			self.gl.EnableVertexAttribArray(color_attrib as bindings::types::GLuint);
 		}
 	}
 	fn draw(&mut self) {
 		println!("Drawing.");
+
 		unsafe {
-            self.gl.ClearColor(1.0, 0.5, 0.7, 1.0);
-            self.gl.Clear(bindings::COLOR_BUFFER_BIT);
-            self.gl.DrawArrays(bindings::TRIANGLES, 0, 3);
+			gl::DrawBuffer( gl::BACK );
+			gl::Clear( gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT );
+
+			gl::MatrixMode( gl::PROJECTION );
+
+			// This is essentially a gluPerspective call
+			let projection: Matrix4<f32> = PerspectiveFov {
+				fovy: Deg(45.0).into(), 
+				aspect: 1.0, 
+				near: 0.1, 
+				far: 1000.0
+			}.into();
+			let arr: &[f32; 16] = projection.as_ref();
+			gl::LoadMatrixf(arr.as_ptr());
+
+
+			gl::MatrixMode( gl::MODELVIEW );
+
+			// This is essentially a gluLookAt call
+			let mat: Matrix4<f32> = Matrix4::look_at(
+				Point3::new(-1.0, 2.0, 3.0), 
+				Point3::new(0.0, 0.0, 0.0),
+				Vector3::new(0.0, 1.0, 0.0)
+			);
+			let arr: &[f32; 16] = mat.as_ref();
+			gl::LoadMatrixf(arr.as_ptr());
+
+			gl::ShadeModel(gl::FLAT);
+
+
+			// rotate the scene:
+			gl::Rotatef(self.yrot, 0.0, 1.0, 0.0);
+			gl::Rotatef(self.xrot, 1.0, 0.0, 1.0);
+
+
+			// uniformly scale the scene:
+			gl::Scalef( self.scale, self.scale, self.scale );
+
+
+			// set the fog parameters:
+			// if self.fog_on {
+			// 	gl::Fogi( gl::FOG_MODE, FOGMODE );
+			// 	gl::Fogfv( gl::FOG_COLOR, FOGCOLOR );
+			// 	gl::Fogf( gl::FOG_DENSITY, FOGDENSITY );
+			// 	gl::Fogf( gl::FOG_START, FOGSTART );
+			// 	gl::Fogf( gl::FOG_END, FOGEND );
+			// 	gl::Enable( gl::FOG );
+			// } else {
+			// 	gl::Disable( gl::FOG );
+			// }
+
+
+			// possibly draw the axes:
+
+			// if self.axis_on {
+			// 	gl::Color3fv( &Colors[WhichColor][0] );
+			// 	gl::CallList( AxesList );
+			// }
+
+			// draw the current object:
+			if let Some(ref lists) = self.display_lists {
+				gl::CallList( lists.cube );
+				self.draw_axis();
+			}
+
+			/*if( DepthFightingOn != 0 )
+			{
+				glPushMatrix( );
+					glRotatef( 90.,   0., 1., 0. );
+					glCallList( BoxList );
+				glPopMatrix( );
+			}*/
+
+
+			// draw some gratuitous text that just rotates on top of the scene:
+
+			// glDisable( gl::DEPTH_TEST );
+			// glColor3f( 0., 1., 1. );
+			// DoRasterString( 0., 1., 0., "Text That Moves" );
+
+
+			// draw some gratuitous text that is fixed on the screen:
+			//
+			// the projection matrix is reset to define a scene whose
+			// world coordinate system goes from 0-100 in each axis
+			//
+			// this is called "percent units", and is just a convenience
+			//
+			// the modelview matrix is reset to identity as we don't
+			// want to transform these coordinates
+
+			// glDisable( gl::DEPTH_TEST );
+			// glMatrixMode( gl::PROJECTION );
+			// glLoadIdentity( );
+			// gluOrtho2D( 0., 100.,     0., 100. );
+			// glMatrixMode( gl::MODELVIEW );
+			// glLoadIdentity( );
+			// glColor3f( 1., 1., 1. );
+			// DoRasterString( 5., 5., 0., "Text That Doesn't" );
+
+			gl::Flush();
         }
 		self.wrapped_context.swap_buffers().unwrap();
+	}
+	fn animate(&mut self) {
+		let now = Instant::now();
+		match self.last_animate {
+			None => {
+				// We haven't animated yet.
+			},
+			Some(last_inst) => {
+				// Diff is the # of miliseconds since the last animate call.
+				let _diff = now.duration_since(last_inst).as_millis();
+				// self.yrot += diff as f32 / 50.0;
+			}
+		}
+		self.last_animate = Some(now);
+
+		self.wrapped_context.window().request_redraw();
 	}
 	fn resized(&mut self, logical_size: &LogicalSize) {
 		let dpi_factor = self.wrapped_context.window().hidpi_factor();
 		self.wrapped_context.resize(logical_size.to_physical(dpi_factor));
+	}
+	fn mouse_move(&mut self, diff_x: f32, diff_y: f32) {
+		if self.button_states.left == ElementState::Pressed {
+			self.yrot -= diff_x;
+			self.xrot -= diff_y;
+		}
+	}
+	fn scroll_delta(&mut self, diff_y: f32) {
+		self.scale += diff_y / 100.0;
+		if self.scale < 0.05 {
+			self.scale = 0.05;
+		}
 	}
 }
 
@@ -134,15 +330,19 @@ fn main() {
     let wb = WindowBuilder::new().with_title("OpenGL / GLUT Sample -- Evan Brass");
 
     let windowed_context =
-        ContextBuilder::new().build_windowed(wb, &event_loop).unwrap();
+        ContextBuilder::new()
+		.with_gl(GlRequest::Specific(OpenGl, (2, 1)))
+		.with_vsync(true)
+		.build_windowed(wb, &event_loop).unwrap();
 	
 	let mut demo = Demo::new(unsafe { windowed_context.make_current().unwrap() });
 
 	demo.init();
+	demo.animate(); // Initialize variables and 
 
     event_loop.run(move |event, _, control_flow| {
-        // println!("{:?}", event);
-        *control_flow = ControlFlow::Wait;
+		println!("{:?}", event);
+        *control_flow = ControlFlow::Poll;
 
         match event {
             Event::LoopDestroyed => return,
@@ -158,6 +358,29 @@ fn main() {
                 }
                 _ => (),
             },
+			Event::DeviceEvent{ ref event, .. } => match event {
+				// Pass mouse movement to the demo:
+				DeviceEvent::MouseMotion{ delta: (x, y) } => demo.mouse_move(*x as f32, *y as f32),
+				// Pass the mouse scroll to the demo:
+				DeviceEvent::MouseWheel{ delta } => match delta {
+					MouseScrollDelta::LineDelta(_, forward) => {
+						let y = -forward;
+						demo.scroll_delta(y);
+					},
+					MouseScrollDelta::PixelDelta(LogicalPosition{ y, .. }) => {
+						demo.scroll_delta(*y as f32);
+					}
+				},
+				// Keep the button states up to date:
+				DeviceEvent::Button{button: 1, state } => demo.button_states.left = *state,
+				DeviceEvent::Button{button: 2, state } => demo.button_states.middle = *state,
+				DeviceEvent::Button{button: 3, state } => demo.button_states.right = *state,
+
+				_ => ()
+			},
+			Event::NewEvents(StartCause::Poll) => {
+				demo.animate();
+			},
             _ => (),
         }
     });
